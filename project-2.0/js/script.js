9346d6e3660d0a62e722a14a602fc9af06279fcb
Gaurrus <listenbulletformyvalentine@gmail.com>
2021-08-05 23:19:45 +0300

    home 05-08-2021

--- project-2.0/js/script.js
+++ project-2.0/js/script.js
@@ -15,11 +15,14 @@
   const burgerItem = document.querySelector('.burger');
   const menu = document.querySelector('.nav');
   const menuCloseItem = document.querySelector('.nav-close')
+  const body = document.querySelector('.body');
   burgerItem.addEventListener('click', () => {
     menu.classList.add('nav-active');
+    body.classList.add('body-unactive');
   });
   menuCloseItem.addEventListener('click', () => {
     menu.classList.remove('nav-active');
+    body.classList.remove('body-unactive');
   });
 }());
 
