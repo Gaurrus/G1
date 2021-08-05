79b129d6bab4c59626c1e775a97baa58b0bc3b8e
Gaurrus <listenbulletformyvalentine@gmail.com>
2021-08-05 10:58:00 +0300

    work 05.08.2021

--- project-2.0/js/script.js
+++ project-2.0/js/script.js
@@ -8,3 +8,20 @@
     }
   };
 }());
+
+//*burger hendler start *//
+
+(function () {
+  const burgerItem = document.querySelector('.burger');
+  const menu = document.querySelector('.nav');
+  const menuCloseItem = document.querySelector('.nav-close')
+  burgerItem.addEventListener('click', () => {
+    menu.classList.add('nav-active');
+  });
+  menuCloseItem.addEventListener('click', () => {
+    menu.classList.remove('nav-active');
+  });
+}());
+
+
+//*burger hendler end *//
