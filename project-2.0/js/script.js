805a847eac9abcfd4ea55c6fb29376470ddcf5d4
Gaurrus <listenbulletformyvalentine@gmail.com>
2021-08-16 15:13:53 +0300

    work 16.08.2021

--- project-2.0/js/script.js
+++ project-2.0/js/script.js
@@ -1,9 +1,6 @@
 (function () {
   const header = document.querySelector('.header');
   document.onscroll = () => {
-    if (window.innerWidth < 768) {
-      header.classList.remove('header_active');
-    } else 
     if (window.pageYOffset > 25) {
       header.classList.add('header_active');
     }
@@ -29,6 +26,21 @@
     body.classList.remove('body-unactive');
   });
 }());
+
+// (function () {
+//   const header = document.querySelector('.header');
+//   document.onscroll = () => {
+//     if (window.innerWidth < 768) {
+//       header.classList.remove('header_active');
+//     } else 
+//     if (window.pageYOffset > 25) {
+//       header.classList.add('header_active');
+//     }
+//     else {
+//       header.classList.remove('header_active');
+//     }
+//   };
+// }());
 
 
 //*burger hendler end *//
