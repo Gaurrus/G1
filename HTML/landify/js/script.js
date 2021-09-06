74b539225b1e9d70d29596ceae0ce31d636d866f
Gaurrus <listenbulletformyvalentine@gmail.com>
2021-09-06 14:56:28 +0300

    home 06-09-2021

--- HTML/landify/js/script.js
+++ HTML/landify/js/script.js
@@ -9,3 +9,18 @@
     }
   };
 }());
+
+(function () {
+  const burgerItem = document.querySelector('.header-burger');
+  const menu = document.querySelector('.header-nav');
+  const menuCloseItem = document.querySelector('.header-nav-close')
+  const body = document.querySelector('.body');
+  burgerItem.addEventListener('click', () => {
+    menu.classList.add('header-nav-active');
+    body.classList.add('body-unactive');
+  });
+  menuCloseItem.addEventListener('click', () => {
+    menu.classList.remove('header-nav-active');
+    body.classList.remove('body-unactive');
+  });
+}());
