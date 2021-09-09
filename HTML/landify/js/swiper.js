b4a77f2a443a89b40cbcf81ae5bdb3041159db7e
Gaurrus <listenbulletformyvalentine@gmail.com>
2021-09-09 13:04:55 +0300

    first-slider

---
+++ HTML/landify/js/swiper.js
@@ -0,0 +1,30 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  simulateTouch: false,
+  keyboard: {
+    enabled: true,
+  },
+
+  autoplay: {
+      delay: 2000,
+  },
+  loop: true,
+  slidesPerView: 3,
+});
