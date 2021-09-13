529f6d683ce13789c109d8d05050070127cf04d2
Gaurrus <listenbulletformyvalentine@gmail.com>
2021-09-13 17:10:14 +0300

    animItemOffset

---
+++ HTML/landify/js/animation.js
@@ -0,0 +1,34 @@
+
+const animItems = document.querySelectorAll('.anim-items')
+
+if (animItems.length > 0) {
+  window.addEventListener ('scroll', animOnScroll);
+  console.log ('anim');
+  function animOnScroll (){
+    for (let index = 0; index < animItems.length; index++) {
+      const animItem = animItems[index];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+      if (animItemHeight > window.innerHeight )
+          animItemPoint = window.innerHeight - window.innerHeight / animStart;
+          
+    }
+
+    if ((pageYOffset > animItemOffset - animItemPoin) && pageYOffset < (animItemOffset + animItemHeight)) {
+      animItem.classList.add('animation-active');
+      
+    } else 
+      animItem.classList.remove('animation-active');
+     
+  }
+  function offset(el) {
+    var rect = el.getBoundingClientRect(),
+    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+}
+}
