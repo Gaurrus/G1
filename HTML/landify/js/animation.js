d8fbe60d3a8486d48673b6bc479d93a28ed331fd
Gaurrus <listenbulletformyvalentine@gmail.com>
2021-09-13 20:39:57 +0300

    alexey-animation-chenges

--- HTML/landify/js/animation.js
+++ HTML/landify/js/animation.js
@@ -1,34 +1,30 @@
 
 const animItems = document.querySelectorAll('.anim-items')
+function offset(el) {
+  console.log(el);
+  var rect = el.getBoundingClientRect(),
+  scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+  scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+}
 
 if (animItems.length > 0) {
-  window.addEventListener ('scroll', animOnScroll);
+  window.addEventListener ('scroll', animOnScroll());
   console.log ('anim');
   function animOnScroll (){
-    for (let index = 0; index < animItems.length; index++) {
-      const animItem = animItems[index];
+    animItems.forEach((animItem) => {
       const animItemHeight = animItem.offsetHeight;
       const animItemOffset = offset(animItem).top;
       const animStart = 4;
-
       let animItemPoint = window.innerHeight - animItemHeight / animStart;
-
-      if (animItemHeight > window.innerHeight )
+      if (animItemHeight > window.innerHeight ){
           animItemPoint = window.innerHeight - window.innerHeight / animStart;
-          
-    }
-
-    if ((pageYOffset > animItemOffset - animItemPoin) && pageYOffset < (animItemOffset + animItemHeight)) {
-      animItem.classList.add('animation-active');
-      
-    } else 
-      animItem.classList.remove('animation-active');
-     
+      }
+      if ((window.pageYOffset > animItemOffset - animItemPoint) && window.pageYOffset < (animItemOffset + animItemHeight)) {
+        animItem.classList.add('animation-active');
+      } else {
+        animItem.classList.remove('animation-active');
+      }}   
+    )   
   }
-  function offset(el) {
-    var rect = el.getBoundingClientRect(),
-    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
-}
 }
