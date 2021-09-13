bfd7139386b428e79ff2c2de1a69f89691bfc453
valadzkoaliaksei <valadzkoaliaksei@tut.by>
2021-09-13 13:17:48 +0300

    переделал слайдер

--- HTML/landify/js/swiper.js
+++ HTML/landify/js/swiper.js
@@ -1,68 +1,68 @@
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  
+let catalogSlider = null;
+const mediaQuerySize = 1040;
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-  simulateTouch: false,
-  keyboard: {
-    enabled: true,
-  },
-
-  autoplay: {
-      delay: 2000,
-  },
-  loop: true,
-
-  slidesPerView: 4,
-
-  spaceBetween: 8,
-
-  breakpoints: {
-    1600: {
-      slidesPerView: 5,
-      autoplay: false,
-    },
-    769: {
-      slidesPerView: 5,
-      autoplay: false,
-    },
-    768: {
-      slidesPerView: 3,
-    },
-
-    651: {
-      slidesPerView: 3,
-    },
-    610: {
-      slidesPerView: 2,
-    },
-
-    450: {
-      slidesPerView: 2,
-    },
-    420: {
-      slidesPerView: 2,
-    },
-    400: {
-      slidesPerView: 1,
-    },
-
-    320: {
-      slidesPerView: 1,
-    }
+function catalogSliderInit() {
+  if (!catalogSlider) {
+    catalogSlider = new Swiper(".swiper", {
+      // Navigation arrows
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      // If we need pagination
+      pagination: {
+        el: ".swiper-pagination",
+      },
+      // And if we need scrollbar
+      scrollbar: {
+        el: ".swiper-scrollbar",
+      },
+      keyboard: {
+        enabled: true,
+      },
+      autoplay: {
+        delay: 2000,
+      },
+      loop: true,
+      spaceBetween: 8,
+      breakpoints: {
+        767: {
+          slidesPerView: 5,
+        },
+        651: {
+          slidesPerView: 3,
+        },
+        420: {
+          slidesPerView: 2,
+        },
+        320: {
+          slidesPerView: 1,
+        },
+      },
+    });
   }
+}
+
+const catalogSliderDestroy = () => {
+  if (catalogSlider) {
+    catalogSlider.destroy();
+    catalogSlider = null;
+  }
+};
+
+const getCotalogSlider = () => {
+  // Если ширина экрана меньше или равна mediaQuerySize(1040)
+  if (window.innerWidth <= mediaQuerySize) {
+    // Инициализировать слайдер
+    catalogSliderInit();
+  } else {
+    // Уничтожить слайдер
+    catalogSliderDestroy();
+  }
+};
+
+getCotalogSlider();
+
+window.addEventListener("resize", () => {
+  getCotalogSlider();
 });
