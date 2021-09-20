4cc2a3bf2b744c78b43a6bbe2016bf9aafcf67f3
Gaurrus <listenbulletformyvalentine@gmail.com>
2021-09-20 21:51:06 +0300

    js-homework-20.09.2021

--- JS/l-3/q-4/q-4-2/script.js
+++ JS/l-3/q-4/q-4-2/script.js
@@ -3,7 +3,12 @@ let width = 1024;
 let imgCol = +prompt("Сколько изображений в галерее?");
 let widthImg = +prompt("Ширина каждого изображниея?");
 let margin = +prompt("Какой отступ?");
-let result = width / widthImg + margin * 2;
+let resWidth = width % (widthImg + margin * 2); //остаток ширины
+let resultImgCol = (width - resWidth) / (widthImg + margin * 2); //количество изображений в ряду
+let colRowImg = (imgCol - (imgCol % resultImgCol)) / resultImgCol; //количесико целых рядов
+console.log(resWidth); 
+console.log(colRowImg); //количесико целых рядов
 
 
-alert('Количество изображенией ' + result);
+alert('Количество изображенией в ряду ' + resultImgCol);
+alert('Количество рядов ' + colRowImg);
