2508709f9f1fa95597b0d398c53082c6d7f6870b
Gaurrus <listenbulletformyvalentine@gmail.com>
2021-09-21 15:51:52 +0300

    js-forEach-from-0-to-imgCol-HOW?

--- JS/l-3/q-4/q-4-2/script.js
+++ JS/l-3/q-4/q-4-2/script.js
@@ -1,38 +1,48 @@
-'use strict'
-let conTainer = document.querySelector('.img-container');
+"use strict";
+let conTainers = document.querySelectorAll(".img-container");
+console.log(conTainers);
 let width = 1024;
 let imgCol = +prompt("Сколько изображений в галерее?");
-console.log(imgCol + ' количество');
-let widthImg = +prompt("Ширина каждого изображниея?");
+console.log(imgCol + " количество");
+if (imgCol <= 20) {
+  let widthImg = +prompt("Ширина каждого изображниея?");
+  let margin = +prompt("Какой отступ?");
+  console.log(margin + " отступ");
+  conTainers.forEach((conTainer) => {
     if (widthImg == 200) {
-      conTainer.classList.add('img-container-200');
-    }
-    else if (widthImg == 250){
-      conTainer.classList.remove('img-container-200');
-      conTainer.classList.add('img-container-250');
-    }
-    else if (widthImg == 300){
-      conTainer.classList.remove('img-container-250');
-      conTainer.classList.add('img-container-300');
-    }
-    else 
-    conTainer.classList.remove('img-container-300');
-console.log(widthImg + ' ширина');
+      conTainer.classList.add("img-container-200");
+    } else if (widthImg == 250) {
+      conTainer.classList.remove("img-container-200");
+      conTainer.classList.add("img-container-250");
+    } else if (widthImg == 300) {
+      conTainer.classList.remove("img-container-250");
+      conTainer.classList.add("img-container-300");
+    } else conTainer.classList.remove("img-container-300");
+    if (margin == 4) {
+      conTainer.classList.add("margin-4");
+    } else conTainer.classList.remove("margin-4");
+    if (margin == 5) {
+      conTainer.classList.add("margin-5");
+    } else conTainer.classList.remove("margin-5");
+    if (margin == 6) {
+      conTainer.classList.add("margin-6");
+    } else conTainer.classList.remove("margin-6");
+  });
 
-let margin = +prompt("Какой отступ?");
-console.log(margin + ' отступ');
-let resWidth = width % (widthImg + margin * 2); //остаток ширины
-console.log(resWidth + ' остаток ширины');
-let resultImgCol = (width - resWidth) / (widthImg + margin * 2); //количество изображений в ряду
-console.log(resultImgCol + ' изо в ряду');
-let colRowImg = (imgCol - (imgCol % resultImgCol)) / resultImgCol; //количесико целых рядов
-console.log(colRowImg + ' целых рядов');
-let resImgLastRow = imgCol - resultImgCol * colRowImg;
-console.log(resImgLastRow + ' в последнем ряду');
-  
+  console.log(widthImg + " ширина");
 
+  let resWidth = width % (widthImg + margin * 2); //остаток ширины
+  console.log(resWidth + " остаток ширины");
+  let resultImgCol = (width - resWidth) / (widthImg + margin * 2); //количество изображений в ряду
+  console.log(resultImgCol + " изо в ряду");
+  let colRowImg = (imgCol - (imgCol % resultImgCol)) / resultImgCol; //количесико целых рядов
+  console.log(colRowImg + " целых рядов");
+  let resImgLastRow = imgCol - resultImgCol * colRowImg;
+  console.log(resImgLastRow + " в последнем ряду");
 
-
-alert('Количество изображенией в ряду ' + resultImgCol);
-alert('Количество целых рядов ' + colRowImg);
-alert('Изображений в последнем ряду ' + resImgLastRow);
+  alert("Количество изображенией в ряду " + resultImgCol);
+  alert("Количество целых рядов " + colRowImg);
+  if (resImgLastRow != 0)
+    alert("Изображений в последнем ряду " + resImgLastRow);
+  else alert("Последний ряд полный");
+} else alert("Превышено допустимое количество изображений!");
