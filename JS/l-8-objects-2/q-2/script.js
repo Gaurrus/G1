85a84a5a27a796f5e6f1465a4d59d1b154973bc8
Gaurrus <listenbulletformyvalentine@gmail.com>
2021-10-12 15:40:38 +0300

    homePC_objects-2_12.10.2021

---
+++ JS/l-8-objects-2/q-2/script.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function Viechle() {
+  this.model = "Тесла";
+  this.engineVolume = "3000";
+  this.year = "2007";
+}
+
+const redViechle = new Viechle("Red");
+
+console.log(redViechle);
