9731fe4317abbbc001eb22924f9101b6d5d578ce
Gaurrus <listenbulletformyvalentine@gmail.com>
2021-10-23 10:27:25 +0300

    homePC_homework_23.10.2021

--- JS/l-12/q-4/script.js
+++ JS/l-12/q-4/script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+const button = document.getElementById("button");
+const body = document.querySelector("body");
+
+button.addEventListener("click", () => {
+  const heading = document.querySelector("h2");
+  if (heading) heading.remove();
+  body.insertAdjacentHTML(
+    "beforeend",
+    `<h2>${max1} - max1,
+    ${max2} - max2,
+    ${max3} - max3, ${rest} </h2>`
+  );
+});
+
+let speed = [9.73, 9.68, 9.71, 9.74, 9.72, 9.75];
+console.log(speed);
+
+const sortSpeed = speed.sort((a, b) => b - a);
+console.log(sortSpeed);
+
+const [max1, max2, max3, ...rest] = sortSpeed;
+console.log(`${max1} - max1`, `${max2} - max2`, `${max3} - max3`, rest);
