88352e23addc51c897916ad1178e081579cb77a5
Gaurrus <listenbulletformyvalentine@gmail.com>
2021-10-31 12:36:59 +0300

    work-pc_dom-queryselector_31.10.

--- JS/l-14/q-1/script.js
+++ JS/l-14/q-1/script.js
@@ -1,5 +1,9 @@
 "use strict";
 
-for (let nodes of document.body.childNodes) {
-  console.log(nodes);
-}
+const redCells = document.querySelectorAll(".table-item");
+console.log(redCells);
+
+redCells.forEach((item, index) => {
+  redCells[4 * index].style.backgroundColor = "red";
+  redCells[6 * index].style.backgroundColor = "red";
+});
