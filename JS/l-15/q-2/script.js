cd265726026a5a5ce92d38cff6f068fbab888fbe
Gaurrus <listenbulletformyvalentine@gmail.com>
2021-11-01 22:41:51 +0300

    homePC_homework_chenging-HTML_01.11.2021

--- JS/l-15/q-2/script.js
+++ JS/l-15/q-2/script.js
@@ -3,8 +3,18 @@
 const list = document.querySelector(`#languages`);
 console.log(list);
 list.hidden = true;
-const fullFunction = document.querySelector(`ol>li:first-of-type ol`);
+const fullFunction = document.querySelectorAll(`ol>li:first-of-type ol>li`);
 console.log(fullFunction);
 
-fullFunction
-document.body.textContent = fullFunction;
+fullFunction.forEach((item) => {
+  document.body.insertAdjacentHTML(`beforeend`, `<p> ${item.textContent} </p>`);
+});
+
+const notFullFunction = document.querySelectorAll(`ol>li:last-of-type ol>li`);
+console.log(notFullFunction);
+
+document.body.insertAdjacentHTML(`beforeend`, `<hr>`)
+
+notFullFunction.forEach((item) => {
+  document.body.insertAdjacentHTML(`beforeend`, `<p> ${item.textContent} </p>`);
+});
--- JS/l-15/q-4/script.js
+++ JS/l-15/q-4/script.js
@@ -1,5 +1,22 @@
 "use strict";
+const table = document.querySelector(`table`);
+const removable = document.querySelector(`.skyblue`);
+removable.remove();
 
-for (let nodes of document.body.childNodes) {
-  console.log(nodes);
-}
+const tbody = document.querySelector(`tbody`);
+tbody.insertAdjacentHTML(
+  "afterbegin",
+  `<tr class = 'added-row'>
+  <td></td>
+  <td colspan="31" align = "center">График дежурст сотрудников на август 2020</td>
+  </tr>`
+);
+
+const beforeend = document.querySelector(`#violet`);
+tbody.insertAdjacentHTML("beforeend", beforeend.outerHTML);
+
+table.setAttribute(`border`, `1`);
+table.setAttribute(`cellspacing`, `0`);
+
+tbody.querySelector(`tr:first-of-type`).setAttribute(`bgcolor`, `yellow`);
+tbody.querySelector(`tr:last-of-type`).setAttribute(`bgcolor`, `yellow`);
