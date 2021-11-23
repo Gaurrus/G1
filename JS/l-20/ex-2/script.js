cf2a4e5dea9319a4c7ba4a165d0e2656ff3f4858
Gaurrus <listenbulletformyvalentine@gmail.com>
2021-11-23 10:41:29 +0300

    допилил домашку с таблице при получении данных с сервера

--- JS/l-20/ex-2/script.js
+++ JS/l-20/ex-2/script.js
@@ -1,26 +1,38 @@
 "use strict";
-const button = document.querySelector(`button`);
 
-const getTableData = async (e) => {
-  e.preventDefault();
-  console.log(`hallo`);
-  const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-  if (response.ok) {
-    const result = await response.json();
-    console.log(result);
-    insertTable(result);
-  }
-  console.log(`error`);
-};
+const button = document.querySelector("button");
 
-button.addEventListener("click", getTableData);
-
-const insertTable = () => {};
-
-const setTr = () => {
-  const tr = document.createElement(`tr`);
-  Object.keys(posts[0]).forEach((key) => {
-    tr.insertAdjacentHTML(`beforeend`, `<th>${key}</th>`);
+const setTr = (post) => {
+  const tr = document.createElement("tr");
+  Object.values(post).forEach((item) => {
+    tr.insertAdjacentHTML("beforeend", `<td>${item}</td>`);
   });
   return tr;
 };
+
+const insertTable = (posts) => {
+  const table = document.createElement("table");
+  const tr = document.createElement("tr");
+
+  Object.keys(posts[0]).forEach((item) => {
+    tr.insertAdjacentHTML("beforeend", `<th>${item}</th>`);
+  });
+  table.append(tr);
+  posts.forEach((post) => {
+    table.append(setTr(post));
+  });
+
+  document.body.append(table);
+};
+
+const getTableData = async (e) => {
+  e.preventDefault();
+  console.log("hallo");
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (response.ok) {
+    const result = await response.json();
+    insertTable(result);
+  } else console.log("error");
+};
+
+button.addEventListener("click", getTableData);
